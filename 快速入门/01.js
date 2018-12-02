79421ad4c9216a146cd18ce013f8acd085a0d7f8
Elson <showjim@aliyun.com>
2018-12-02 20:36:18 +0800

    数据类型和变量

--- 快速入门/01.js
+++ 快速入门/01.js
@@ -78,5 +78,43 @@ isNaN(NaN);//true
 null 空值
 undefined 未定义
  */
-
-
+/*
+数组是一组按顺序排列的集合，集合的每个值称为元素。
+JavaScript数组可以包含任意数据类型。
+ */
+var arr = [1,2,3.14,'Hello',null,true];
+new Array(1,2,3);//创建了数组[1,2,3]
+arr[0];//返回索引为0的元素，即1
+arr[5];//返回索引为5的元素，即true
+arr[6];//索引超出了范围，返回undefined
+/* 
+对象，一组由键-值得无序集合
+ */
+var person= {
+    name:'Bob',
+    age:20,
+    tags:['js','web','moblie'],
+    city:'Beijing',
+    hasCar:true,
+    zipcode:null
+};
+// 获取一个对象的属性，对象变量.属性名
+person.name;//'Bob'
+person.zipcode;//null
+/* 
+变量名是大小写英文、数字、$和_的组合，且不能用数字开头
+动态语言，变量本身类型不固定
+静态语言，定义变量时必须指定变量类型
+ */
+var a;
+var $b=1;
+var s_007='007';
+var Answer=true;
+var t=null;
+a=123;
+a='ABC';
+var x=10;
+x=x+2;
+/* 
+strict模式，未使用var声明变量就使用的，将导致运行错误
+ */
